2f09205bba1777416fa2511bb8ca1f640ad1fcd0
Korode Ogunfowote <koroskki@gmail.com>
2024-02-10 01:58:55 +0100

    Update axiosConfig.ts

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,29 +1,8 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 export const authAxios = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
-
-authAxios.interceptors.response.use(
-    (response) => response,
-    (error: AxiosError) => {
-        /**
-         * Do something in case the response returns an error code [3**, 4**, 5**] etc
-         * For example, on token expiration retrieve a new access token, retry a failed request etc
-         */
-        const { response } = error;
-
-        if (!response) {
-            console.log("Internet connection is slow");
-        }
-
-        // if (response && response.status === 403) {
-        //   return (window.location.href = '/forbidden');
-        // }
-
-        if (response && response.status === 401) {
-            return (window.location.href = "/auth/login");
-        }
-        return Promise.reject(error);
-    }
-);

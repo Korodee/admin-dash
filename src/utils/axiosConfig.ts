f1fd3c907cb2fb5255de8a29c21388f39d78303d
Korode Ogunfowote <koroskki@gmail.com>
2024-02-10 01:30:28 +0100

    Update axiosConfig.ts

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,75 +1,31 @@
-// import { callConfig } from "@/components/Layout/AppLayout";
-// import { logout } from "@/redux/slices/auth";
-// import { checkAuthToken } from "@/services/authService";
-import axios from "axios";
-import { Dispatch } from 'react';
+import axios, { AxiosError } from "axios";
 
-export const authAxios = axios.create({
+const authAxios = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-    responseType: 'json',
 });
 
-export const publicAxios = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-    responseType: 'json',
-});
+authAxios.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+        /**
+         * Do something in case the response returns an error code [3**, 4**, 5**] etc
+         * For example, on token expiration retrieve a new access token, retry a failed request etc
+         */
+        const { response } = error;
 
-// Add a response interceptor
-authAxios.interceptors.response.use(function (response) {
-    return response;
-}, function (error) {
-    return Promise.reject(error);
-});
-  
-// Add a response interceptor
-authAxios.interceptors.request.use(function (config:any) {
-    let authToken = JSON.parse(localStorage.getItem( 'auth_token') as string).token
-    config.headers['Authorization'] = 'Bearer ' + authToken;
-    return config;
-}, function (error) {
-    return Promise.reject(error);
-});
-  
-// authAxios.interceptors.request.use(async (req: any) => {
-//     const state = callConfig.call?.state;
-//     const token = state?.Auth.token;
-//     const dispatch= callConfig.call.dispatch
+        if (!response) {
+            console.log("Internet connection is slow");
+        }
 
-//     if(!token){
-//         return dispatch?.(logout({}))
-//     }
+        // if (response && response.status === 403) {
+        //   return (window.location.href = '/forbidden');
+        // }
 
-//     try {
-//         const {
-//             newToken,
-//             oldToken,
-//             logout: shouldLogout,
-//         } = await checkAuthToken(dispatch as Dispatch<any>, token);
+        if (response && response.status === 401) {
+            return (window.location.href = "/auth/login");
+        }
+        return Promise.reject(error);
+    }
+);
 
-//         if (newToken) {
-//             req.headers.authorization = `Bearer ${newToken}`;
-//             return req;
-//         }
-
-//         if (oldToken) {
-//             return req;
-//         }
-
-//         if (shouldLogout) {
-//             const controller = new AbortController();
-//             const conf = {
-//                 ...req,
-//                 signal: controller.signal,
-//             };
-
-//             controller.abort();
-//             dispatch?.(logout({}));
-
-//             return conf;
-//         }
-//     } catch (error) {
-//         return dispatch?.(logout({}));
-//     }
-
-//     return req;
-// });
+export default authAxios;
